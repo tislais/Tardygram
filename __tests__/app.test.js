caffda3e9486ee461ee1d44115ea3f1e2b18d8dd
Tis Lais <tistististis@gmail.com>
2021-06-29 17:38:47 -0700

    Add failing post test for user sign up

---
+++ __tests__/app.test.js
@@ -0,0 +1,28 @@
+import pool from '../lib/utils/pool.js';
+import setup from '../data/setup.js';
+import request from 'supertest';
+import app from '../lib/app.js';
+
+describe('demo routes', () => {
+  beforeAll(() => {
+    return setup(pool);
+  });
+
+  it('signs a user up via POST', async () => {
+    
+    const res = await request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'test@example.com',
+        password: 'supersecretpizzaparty'
+      });
+
+    const expected = {
+      id: '1',
+      email: 'test@example.com'
+    };
+    
+    expect(res.body).toEqual(expected);
+  });
+
+});
