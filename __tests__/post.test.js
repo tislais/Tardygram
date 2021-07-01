716fb160afd681a593d57901a97f12d86a4f5bab
Tis Lais <tistististis@gmail.com>
2021-06-30 17:03:56 -0700

    Add failing get all posts test

--- __tests__/post.test.js
+++ __tests__/post.test.js
@@ -9,6 +9,18 @@ describe('tardygram post routes', () => {
   let agent;
   let user;
 
+  const post1 = {
+    photoUrl: 'cat.png',
+    caption: 'my cat',
+    tags: ['#cute', '#cat'] 
+  };
+
+  const post2 = {
+    photoUrl: 'frog.png',
+    caption: 'frog I found',
+    tags: ['#freg', '#forg'] 
+  };
+
   beforeEach(async () => {
     await setup(pool);
     agent = await request.agent(app);
@@ -24,21 +36,30 @@ describe('tardygram post routes', () => {
       });
   });
 
-  it('creates a post from a known user via post', async () => {
-    const res = await agent.post('/api/v1/posts')
-      .send({
-        userId: user.id,
-        photoUrl: 'cat.png',
-        caption: 'my cat',
-        tags: ['#cute', '#cat']
-      });
+  it('creates a post from a known user via POST', async () => {
+    const res = await agent.post('/api/v1/posts').send({ ...post1, userId: user.id });
 
     const expected = {
       id: '1',
       userId: user.id,
       photoUrl: 'cat.png',
       caption: 'my cat',
-      tags: ['#cute', '#cat']}
+      tags: ['#cute', '#cat']
+    };
+    
+    expect(res.body).toEqual(expected);
+  });
+
+  it('gets all posts via GET', async () => {
+    
+    await agent.post('/api/v1/posts').send({ ...post1, userId: user.id });
+    await agent.post('/api/v1/posts').send({ ...post2, userId: user.id });
+
+    const res = await agent.get('/api/v1/posts');
+    const expected = [
+      { ...post1, id: '1', userId: '1' }, 
+      { ...post2, id: '2', userId: '1' }
+    ];
     
     expect(res.body).toEqual(expected);
   });
